ed50502df779af90864f3d7a6368906e39f17af7
CrisViolencia <david.lapras.cristian75@gmail.com>
2025-06-03 17:24:18 -0500

    Add files via upload

--- python 11-04 cris/inicio.py
+++ python 11-04 cris/inicio.py
@@ -1,1 +1,14 @@
-print("probando ando")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route ('/')
+def index():
+    return render_template ('index.html')
+
+@app.route ('/contact')
+def index():
+    return render_template ('contact.html')
+
+if __name__ == ""__main__"":
+    app.run()           
